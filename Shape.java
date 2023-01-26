4bdd6b5af3d23b21556aee98726137f764f5c14e
AhmadHoumany <55434841+AhmadHoumany@users.noreply.github.com>
2023-01-26 17:14:31 +0200

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,8 @@
+public class Shape {
+ 
+    public static void main(String[] args) {
+
+        System.out.println("   J    a   v     v  a ");
+        System.out.println("   J   a a   v   v  a a");
+    }
+}
