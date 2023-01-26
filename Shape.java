9e1902f4ddd24e78abb5f964b5d69c84235409e5
charbelkhabbaz <ckhabbaz7@gmail.com>
2023-01-26 19:48:50 +0200

    Shape is done by Charbel khabbaz

--- Shape.java
+++ Shape.java
@@ -4,5 +4,8 @@ public class Shape {
 
         System.out.println("   J    a   v     v  a ");
         System.out.println("   J   a a   v   v  a a");
+	System.out.println("J  J  aaaaa   v v  aaaaa");
+	System.out.println(" JJ  a     a   v  a     a");
+
     }
 }
